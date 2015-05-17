50fc801d2ef88bf366a8bce97b304da52bed4815
Zach Kazanski <kazanski.zachary@gmail.com>
2015-05-16 20:42:41 -0400

    wsgi

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from app import app
+
+if __name__ == "__main__":
+    # I really don't care that this is on Github for this site
+    app.secret_key = "dlhwlk2he22efhfj2hlef2ljkfh2lkfeh2h0180"
+    app.run()
